5596622f29d6383f23e7e26756681bceda5e7d89
saima-salar <157168170+saima-salar@users.noreply.github.com>
2025-04-01 06:47:25 +0500

    app.py

--- Assignments 1 to 6/Project 7: Password Generator Python Project/app.py
+++ Assignments 1 to 6/Project 7: Password Generator Python Project/app.py
@@ -1,1 +1,16 @@
+import random
+import string
+
+def generate_password(length=12):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+# User input
+try:
+    length = int(input("🔐 Enter the length of your desired password: "))
+    password = generate_password(length)
+    print("✅ Your Generated Password: ", password)
+except ValueError:
+    print("⚠️ Please enter a valid number!")
 
