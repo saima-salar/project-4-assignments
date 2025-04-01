c1942dcb160969b338cd562c848e5d69e5a1c0c7
saima-salar <157168170+saima-salar@users.noreply.github.com>
2025-04-01 06:24:37 +0500

    madlib.py

--- Assignments 1 to 6/Project 1: Mad libs Python Project/madlib.py
+++ Assignments 1 to 6/Project 1: Mad libs Python Project/madlib.py
@@ -1,1 +1,16 @@
 
+
+    # user input
+name = input("Enter the boy name:")
+programming_language = input("Enter a programming language (e.g: python,next.js,typscript,html):")
+mentor = input("Enter your mentor name:")
+location = input("Enter your location")
+
+#story created
+print("\n Here is my little story baed on mad libs game!")
+print(f"Once upon a time,there was a boy named {name}.")
+print(f"{name} was very curious and always want to learn new things")
+print(f"One day, he decided to learn {programming_language} language at {location}")
+print(f"Luckily {name} found a great mentor named {mentor} who was an expert in {programming_language}.")
+print("And so , his journey continues...!")
+
